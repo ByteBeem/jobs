178e55b802c1aed0a8a1d5e1781dde858cc461f9
mohla <donald.mxolisi@proton.me>
2024-06-27 21:32:39 +0200

    new

--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -2,11 +2,16 @@ import React from 'react';
 import './JobCard.scss';
 
 const JobCard = ({ job }) => {
+
+  const handleApplyClick = () => {
+    window.location.href = job.Link; 
+  };
+
   return (
     <div className="job-card">
       <h2>{job.title}</h2>
       <p>{job.description}</p>
-      <button className="apply-button">Apply</button>
+      <button className="apply-button" onClick={handleApplyClick}>Apply</button>
     </div>
   );
 };
